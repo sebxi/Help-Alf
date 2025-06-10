552365307235c0fc540cc05b1f195d9d99d52641
Sebastian <anderseka.sebastian@yahoo.com>
2025-06-10 20:49:24 +0200

    Ungültige Maps -> Spaceship lost forever.

--- spec/findSpaceship.test.js
+++ spec/findSpaceship.test.js
@@ -5,8 +5,21 @@ describe("HelpAlf", function () {
     helpAlf = new HelpAlf();
   });
 
+  //Leere Map
   it("should return Spaceship lost forever", function () {
     const map = "";
+    expect(helpAlf.findSpaceship(map)).toEqual("Spaceship lost forever.");
+  });
+
+  //Üngültige Map
+  it("should return Spaceship lost forever", function () {
+    const map = "keine Map";
+    expect(helpAlf.findSpaceship(map)).toEqual("Spaceship lost forever.");
+  });
+  
+  //Map mit nur einem Platz (Ungültig)
+  it("should return Spaceship lost forever", function () {
+    const map = ".";
     expect(helpAlf.findSpaceship(map)).toEqual("Spaceship lost forever.");
   });
 });
